128df5302a237153990e0ca05bdb19c24463cdf8
Ray <vecum0814@naver.com>
2020-10-04 13:39:51 +0900

    HW1

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const http = require('http');
 const hostname = '127.0.0.1'; const port = 4000;
-var d = new Date();
-var n = d.toString();
-
+const crtTime = new Date();
 const server = http.createServer((req, res) => { 
     res.statusCode = 200; res.setHeader('Content-Type', 'text/plain'); 
-    res. write("Currnt date info is: " + n);
-    res.end('\n');
+    res.end(crtTime.toString());
 });
 server.listen(port, hostname, () => {
 console.log(`Server running at http://${hostname}:${port}/`);
